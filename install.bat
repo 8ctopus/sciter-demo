b09d0593685e3dfa3389e72d4756e6fb5eef8ce9
8ctopus <hello@octopuslabs.io>
2021-04-27 11:16:20 +0500

    updated: sciter sdk to 4.4.6.8

--- install.bat
+++ install.bat
@@ -1,12 +1,14 @@
-; sciter 4.4.6.7
-curl -LO https://github.com/c-smile/sciter-js-sdk/raw/4b10c90a4efd3ac19a07a78e56469e920c078761/bin/windows/x32/scapp.exe
-curl -LO https://github.com/c-smile/sciter-js-sdk/raw/4b10c90a4efd3ac19a07a78e56469e920c078761/bin/windows/x32/sciter.dll
-curl -LO https://github.com/c-smile/sciter-js-sdk/raw/4b10c90a4efd3ac19a07a78e56469e920c078761/bin/windows/x32/inspector.exe
+; sciter 4.4.6.8
+curl -LO https://github.com/c-smile/sciter-js-sdk/raw/c27c5679d79ee94db8730ce06a7cda2bf5d3daba/bin/windows/x32/scapp.exe
+curl -LO https://github.com/c-smile/sciter-js-sdk/raw/c27c5679d79ee94db8730ce06a7cda2bf5d3daba/bin/windows/x32/sciter.dll
+curl -LO https://github.com/c-smile/sciter-js-sdk/raw/c27c5679d79ee94db8730ce06a7cda2bf5d3daba/bin/windows/x32/inspector.exe
 
 pause
+exit
 
 ; master
 ;curl -LO https://github.com/c-smile/sciter-sdk/raw/master/bin.win/x32/scapp.exe
 ;curl -LO https://github.com/c-smile/sciter-sdk/raw/master/bin.win/x32/sciter.dll
 ;curl -LO https://github.com/c-smile/sciter-sdk/raw/master/bin.win/x32/inspector.exe
+
 
