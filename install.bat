477767bbf71db335c1b619d08e53ff82cd7e211d
8ctopus <hello@octopuslabs.io>
2021-04-27 11:13:41 +0500

    improved: install bat uses tested sciter version 4.4.6.7

--- install.bat
+++ install.bat
@@ -1,3 +1,12 @@
-curl -LO https://github.com/c-smile/sciter-sdk/raw/master/bin.win/x32/scapp.exe
-curl -LO https://github.com/c-smile/sciter-sdk/raw/master/bin.win/x32/sciter.dll
-curl -LO https://github.com/c-smile/sciter-sdk/raw/master/bin.win/x32/inspector.exe
+; sciter 4.4.6.7
+curl -LO https://github.com/c-smile/sciter-js-sdk/raw/4b10c90a4efd3ac19a07a78e56469e920c078761/bin/windows/x32/scapp.exe
+curl -LO https://github.com/c-smile/sciter-js-sdk/raw/4b10c90a4efd3ac19a07a78e56469e920c078761/bin/windows/x32/sciter.dll
+curl -LO https://github.com/c-smile/sciter-js-sdk/raw/4b10c90a4efd3ac19a07a78e56469e920c078761/bin/windows/x32/inspector.exe
+
+pause
+
+; master
+;curl -LO https://github.com/c-smile/sciter-sdk/raw/master/bin.win/x32/scapp.exe
+;curl -LO https://github.com/c-smile/sciter-sdk/raw/master/bin.win/x32/sciter.dll
+;curl -LO https://github.com/c-smile/sciter-sdk/raw/master/bin.win/x32/inspector.exe
+
