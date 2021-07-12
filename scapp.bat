894beb83b1ebe1c7d829bb228db373fe7a3f954f
8ctopus <hello@octopuslabs.io>
2021-07-12 12:20:06 +0500

    fixed: scapp bat would not close existing inspector

--- scapp.bat
+++ scapp.bat
@@ -1,3 +1,6 @@
+tasklist /fi "ImageName eq inspector.exe" /fo csv 2>NUL | find /I "inspector.exe">NUL
+if "%ERRORLEVEL%"=="0" taskkill /IM inspector.exe /f
+
 start inspector.exe
 
 tasklist /fi "ImageName eq scapp.exe" /fo csv 2>NUL | find /I "scapp.exe">NUL
