42ee8fd217e46747eed41f592fa481e0bd24aef7
8ctopus <hello@octopuslabs.io>
2021-07-12 12:20:49 +0500

    fixed: maximize window on refresh

--- js/utils.js
+++ js/utils.js
@@ -14,24 +14,14 @@ document.on("ready", function() {
     Window.this.isTopmost = true;
     Window.this.isTopmost = false;
 
-/*
     // get screen dimensions
     const [sx, sy, sw, sh] = Window.this.screenBox("frame");
 
     //console.log(`${sx} ${sy} ${sw} ${sh}`)
 
-    // get window dimensions with border
-    const [wx, wy, ww, wh] = Window.this.box("rectw", "border");
+    // move and maximize window
+    Window.this.move(0, 0, sw, sh);
 
-    //console.log(`${wx} ${wy} ${ww} ${wh}`)
-
-    // calculate position
-    const left = (sw + 1 - ww) / 2;
-    const top  = (sh + 1 - wh) / 2;
-
-    // move window
-    Window.this.move(left, top);
-*/
     // set focus
     document.body.state.focus = true;
 });
