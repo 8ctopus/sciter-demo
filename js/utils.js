b221c19715c4c68f829ef345c3679d9c11613f27
8ctopus <hello@octopuslabs.io>
2021-07-13 12:58:17 +0500

    improved: code how to maximize the window

--- js/utils.js
+++ js/utils.js
@@ -8,20 +8,3 @@ document.on("keydown", function(event, element) {
     // reload app
     Window.this.load(location.href);
 });
-
-document.on("ready", function() {
-    // bring window to front
-    Window.this.isTopmost = true;
-    Window.this.isTopmost = false;
-
-    // get screen dimensions
-    const [sx, sy, sw, sh] = Window.this.screenBox("frame");
-
-    //console.log(`${sx} ${sy} ${sw} ${sh}`)
-
-    // move and maximize window
-    Window.this.move(0, 0, sw, sh);
-
-    // set focus
-    document.body.state.focus = true;
-});
