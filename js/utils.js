1f6085c5dd8b11cbf2700cede110bc417264ea0b
8ctopus <hello@octopuslabs.io>
2021-07-13 15:54:49 +0500

    improved: window reload remaximizes the app

--- js/utils.js
+++ js/utils.js
@@ -8,3 +8,13 @@ document.on("keydown", function(event, element) {
     // reload app
     Window.this.load(location.href);
 });
+
+document.on("ready", function() {
+    //console.log("Maximize window...");
+
+    if (Window.this.state == Window.WINDOW_MAXIMIZED)
+        Window.this.state = Window.WINDOW_MINIMIZED;
+
+    // maximize window
+    Window.this.state = Window.WINDOW_MAXIMIZED;
+});
