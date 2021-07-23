db25d82ed5cc1c9c75f453e2f0eb48e5137b863a
8ctopus <hello@octopuslabs.io>
2021-07-23 10:51:48 +0500

    cleanup: code

--- js/utils.js
+++ js/utils.js
@@ -2,17 +2,14 @@
 
 // add scapp refresh
 document.on("keydown", function(event, element) {
-    if (event.code !== "KeyF5")
-        return;
-
-    // reload app
-    Window.this.load(location.href);
+    if (event.code === "KeyF5")
+        // reload app
+        Window.this.load(location.href);
 });
 
 document.on("ready", function() {
-    //console.log("Maximize window...");
-
-    if (Window.this.state == Window.WINDOW_MAXIMIZED)
+    // fix issues with window not maxmizing itself on f5
+    if (Window.this.state === Window.WINDOW_MAXIMIZED)
         Window.this.state = Window.WINDOW_MINIMIZED;
 
     // maximize window
