0fe53e9949b9caac5fa65c24b6b0f40416b62b52
8ctopus <hello@octopuslabs.io>
2021-08-03 15:33:18 +0500

    improved: wait for inspector to fully start

--- scapp.bat
+++ scapp.bat
@@ -3,6 +3,9 @@ if "%ERRORLEVEL%"=="0" taskkill /IM inspector.exe /f
 
 start inspector.exe
 
+REM wait for inspector to initialize
+sleep 1
+
 tasklist /fi "ImageName eq scapp.exe" /fo csv 2>NUL | find /I "scapp.exe">NUL
 if "%ERRORLEVEL%"=="0" taskkill /IM scapp.exe /f
 
