041272f685cdb0b36e4d766a625c2ddf18df4a2f
8ctopus <hello@octopuslabs.io>
2021-08-05 10:11:48 +0500

    improved: dialogs converted to module

--- js/dialogs.js
+++ js/dialogs.js
@@ -1,23 +1,23 @@
 // JSX
 // require babel syntax highlighting
 
-const dialogInfo = <info>Hello world!</info>;
+export const dialogInfo = <info>Hello world!</info>;
 
-const dialogAlert = (
+export const dialogAlert = (
     <alert caption="Alert">
         <p>Don't worry!</p>
         <p>It is just a demo</p>
     </alert>
 );
 
-const dialogError = (
+export const dialogError = (
     <error>
         <p>Don't worry!</p>
         <p>It is just a demo</p>
     </error>
 );
 
-const dialogQuestion = (
+export const dialogQuestion = (
     <question>
         <content>Be or not to be?</content>
         <buttons>
