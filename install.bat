76cebd0c7035acd186e99ca5ae614ff837dfe03d
8ctopus <hello@octopuslabs.io>
2021-11-01 14:20:27 +0500

    updated: directory architecture for future mac version

--- install.bat
+++ install.bat
@@ -1,7 +1,13 @@
+mkdir bin\win-x32
+
+cd bin\win-x32
+
 REM sciter 4.4.8.15
 curl -LO https://github.com/c-smile/sciter-js-sdk/raw/faeba319c38bac2e833cbf0fe5a6be60cf87a24e/bin/windows/x32/scapp.exe
 curl -LO https://github.com/c-smile/sciter-js-sdk/raw/faeba319c38bac2e833cbf0fe5a6be60cf87a24e/bin/windows/x32/inspector.exe
 curl -LO https://github.com/c-smile/sciter-js-sdk/raw/faeba319c38bac2e833cbf0fe5a6be60cf87a24e/bin/windows/x32/sciter.dll
+
+cd ..
 
 REM sciter package manager 0.2.1
 curl -LO https://github.com/8ctopus/sciter-package-manager/releases/download/0.2.1/spm.phar
--- scapp.bat
+++ scapp.bat
@@ -1,7 +1,7 @@
 tasklist /fi "ImageName eq inspector.exe" /fo csv 2>NUL | find /I "inspector.exe">NUL
 if "%ERRORLEVEL%"=="0" taskkill /IM inspector.exe /f
 
-start inspector.exe
+start bin\win-x32\inspector.exe
 
 REM wait for inspector to initialize
 sleep 1
@@ -9,4 +9,4 @@ sleep 1
 tasklist /fi "ImageName eq scapp.exe" /fo csv 2>NUL | find /I "scapp.exe">NUL
 if "%ERRORLEVEL%"=="0" taskkill /IM scapp.exe /f
 
-start scapp.exe scapp.html --debug
+start bin\win-x32\scapp.exe scapp.html --debug
