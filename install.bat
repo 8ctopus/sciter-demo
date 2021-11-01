5499653b2a7b9431e5bec3f724e44934f35a9dbf
8ctopus <hello@octopuslabs.io>
2021-11-01 14:17:21 +0500

    updated: sciter sdk to 4.4.8.15

--- install.bat
+++ install.bat
@@ -1,7 +1,7 @@
-REM sciter 4.4.8.10
-curl -LO https://github.com/c-smile/sciter-js-sdk/raw/cb9daceb84f6e49e56dee816b4de7d5ddb2829e1/bin/windows/x32/scapp.exe
-curl -LO https://github.com/c-smile/sciter-js-sdk/raw/cb9daceb84f6e49e56dee816b4de7d5ddb2829e1/bin/windows/x32/inspector.exe
-curl -LO https://github.com/c-smile/sciter-js-sdk/raw/cb9daceb84f6e49e56dee816b4de7d5ddb2829e1/bin/windows/x32/sciter.dll
+REM sciter 4.4.8.15
+curl -LO https://github.com/c-smile/sciter-js-sdk/raw/faeba319c38bac2e833cbf0fe5a6be60cf87a24e/bin/windows/x32/scapp.exe
+curl -LO https://github.com/c-smile/sciter-js-sdk/raw/faeba319c38bac2e833cbf0fe5a6be60cf87a24e/bin/windows/x32/inspector.exe
+curl -LO https://github.com/c-smile/sciter-js-sdk/raw/faeba319c38bac2e833cbf0fe5a6be60cf87a24e/bin/windows/x32/sciter.dll
 
 REM sciter package manager 0.2.1
 curl -LO https://github.com/8ctopus/sciter-package-manager/releases/download/0.2.1/spm.phar
