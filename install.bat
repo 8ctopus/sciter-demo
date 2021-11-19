be6013414733cbae8a6c392e0a8d02f953adef76
8ctopus <hello@octopuslabs.io>
2021-11-19 11:33:53 +0500

    improved: install scripts

--- install.bat
+++ install.bat
@@ -1,15 +1,15 @@
+REM sciter 4.4.8.17bis
+SET sciterSDK="be2be52df10ebe501f75901df8ef2467ed710d00"
+
 mkdir bin\win-x32
 
 cd bin\win-x32
 
-REM sciter 4.4.8.15
-curl -LO https://github.com/c-smile/sciter-js-sdk/raw/faeba319c38bac2e833cbf0fe5a6be60cf87a24e/bin/windows/x32/scapp.exe
-curl -LO https://github.com/c-smile/sciter-js-sdk/raw/faeba319c38bac2e833cbf0fe5a6be60cf87a24e/bin/windows/x32/inspector.exe
-curl -LO https://github.com/c-smile/sciter-js-sdk/raw/faeba319c38bac2e833cbf0fe5a6be60cf87a24e/bin/windows/x32/sciter.dll
+curl -LO https://github.com/c-smile/sciter-js-sdk/raw/%sciterSDK%/bin/windows/x32/scapp.exe
+curl -LO https://github.com/c-smile/sciter-js-sdk/raw/%sciterSDK%/bin/windows/x32/inspector.exe
+curl -LO https://github.com/c-smile/sciter-js-sdk/raw/%sciterSDK%/bin/windows/x32/sciter.dll
 
 cd ..
 
-REM sciter package manager 0.2.1
-curl -LO https://github.com/8ctopus/sciter-package-manager/releases/download/0.2.1/spm.phar
-
-pause
+REM sciter package manager 0.2.2
+curl -LO https://github.com/8ctopus/sciter-package-manager/releases/download/0.2.2/spm.phar
--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
+# sciter 4.4.8.17bis
+sciterSDK="be2be52df10ebe501f75901df8ef2467ed710d00"
+
+# get os name
 uname="$(uname -s)"
 
 case "${uname}" in
@@ -25,10 +29,9 @@ case "${machine}" in
 
     cd bin/win-x32
 
-    # sciter 4.4.8.15
-    curl -LO https://github.com/c-smile/sciter-js-sdk/raw/faeba319c38bac2e833cbf0fe5a6be60cf87a24e/bin/windows/x32/scapp.exe
-    curl -LO https://github.com/c-smile/sciter-js-sdk/raw/faeba319c38bac2e833cbf0fe5a6be60cf87a24e/bin/windows/x32/inspector.exe
-    curl -LO https://github.com/c-smile/sciter-js-sdk/raw/faeba319c38bac2e833cbf0fe5a6be60cf87a24e/bin/windows/x32/sciter.dll
+    curl -LO https://github.com/c-smile/sciter-js-sdk/raw/${sciterSDK}/bin/windows/x32/scapp.exe
+    curl -LO https://github.com/c-smile/sciter-js-sdk/raw/${sciterSDK}/bin/windows/x32/inspector.exe
+    curl -LO https://github.com/c-smile/sciter-js-sdk/raw/${sciterSDK}/bin/windows/x32/sciter.dll
     ;;
 
 "linux")
@@ -36,9 +39,9 @@ case "${machine}" in
 
     cd bin/linux
 
-    curl -LO https://github.com/c-smile/sciter-js-sdk/raw/faeba319c38bac2e833cbf0fe5a6be60cf87a24e/bin/linux/x64/scapp
-    curl -LO https://github.com/c-smile/sciter-js-sdk/raw/faeba319c38bac2e833cbf0fe5a6be60cf87a24e/bin/linux/x64/inspector
-    curl -LO https://github.com/c-smile/sciter-js-sdk/raw/faeba319c38bac2e833cbf0fe5a6be60cf87a24e/bin/linux/x64/libsciter-gtk.so
+    curl -LO https://github.com/c-smile/sciter-js-sdk/raw/${sciterSDK}/bin/linux/x64/scapp
+    curl -LO https://github.com/c-smile/sciter-js-sdk/raw/${sciterSDK}/bin/linux/x64/inspector
+    curl -LO https://github.com/c-smile/sciter-js-sdk/raw/${sciterSDK}/bin/linux/x64/libsciter-gtk.so
 
     # make binaries executable
     chmod +x scapp inspector libsciter-gtk.so
@@ -46,19 +49,19 @@ case "${machine}" in
 
 "macosx")
     # download the whole archive because of inspector.app which is a directory
-    curl -LO https://github.com/c-smile/sciter-js-sdk/archive/faeba319c38bac2e833cbf0fe5a6be60cf87a24e.zip
+    curl -LO https://github.com/c-smile/sciter-js-sdk/archive/${sciterSDK}.zip
 
     # unzip binaries
-    unzip faeba319c38bac2e833cbf0fe5a6be60cf87a24e.zip sciter-js-sdk-faeba319c38bac2e833cbf0fe5a6be60cf87a24e/bin/${machine}/* -d .
+    unzip ${sciterSDK}.zip sciter-js-sdk-${sciterSDK}/bin/${machine}/* -d .
 
     # move binaries
-    mv sciter-js-sdk-faeba319c38bac2e833cbf0fe5a6be60cf87a24e/bin .
+    mv sciter-js-sdk-${sciterSDK}/bin .
 
     # delete old dir
-    rmdir sciter-js-sdk-faeba319c38bac2e833cbf0fe5a6be60cf87a24e
+    rmdir sciter-js-sdk-${sciterSDK}
 
     # delete zip
-    rm faeba319c38bac2e833cbf0fe5a6be60cf87a24e.zip
+    rm ${sciterSDK}.zip
 
     cd bin/${machine}
 
