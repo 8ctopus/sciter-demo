17ab7f3cb586728481d380ea6e4da7c50caf27e5
8ctopus <hello@octopuslabs.io>
2022-01-21 11:33:24 +0500

    cleanup: linted code

--- js/dialogs.js
+++ js/dialogs.js
@@ -23,9 +23,9 @@ export const dialogQuestion = (
     <question>
         <content>Be or not to be?</content>
         <buttons>
-            <button #yes role="default-button">Yes</button>
-            <button #no role="cancel-button">No</button>
-            <button #cancel role="cancel-button">Cancel</button>
+            <button id="yes" role="default-button">Yes</button>
+            <button id="no" role="cancel-button">No</button>
+            <button id="cancel" role="cancel-button">Cancel</button>
         </buttons>
     </question>
 );
